0af61255200ddf1f3e37acb1a7b84278fc7ee650
Phil Tek <venjitsu66@gmail.com>
2018-03-04 20:36:41 +0000

    Add date string regex to validator

--- lib/ingest/epg/date/validator/validator.rb
+++ lib/ingest/epg/date/validator/validator.rb
@@ -7,6 +7,10 @@ module Ingest
           Date::abbreviated_day_names.include?(string)
         end
 
+        def self.string_matches_date_string_regex(string)
+          /^[A-Za-z]{3}\s[0-9]{1,2}\s[A-Za-z]{3}\s[0-9]{2}/ =~ string
+        end
+
       end
     end
   end
--- spec/lib/ingest/epg/date/validator/validator_spec.rb
+++ spec/lib/ingest/epg/date/validator/validator_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe do
   describe Ingest::EPG::Date::Validator do
     let(:validator) { Ingest::EPG::Date::Validator }
 
+    describe 'string_matches_date_string_regex' do
+      it 'returns true when given a valid date string' do
+        expect(validator.string_matches_date_string_regex("Mon 1 Jan 2018")).to eq(0)
+      end
+    end
+
     describe '.is_a_day_of_the_week?' do
       context 'when given a valid day of the week' do
         it 'returns true' do
