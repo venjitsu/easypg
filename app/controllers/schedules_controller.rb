88cb7454dc4e664546f675138452f3d08ed4c1c9
Phil Tek <venjitsu66@gmail.com>
2018-03-05 23:24:20 +0000

    Add basic index and show schedule events

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,23 @@
+class SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @schedules = Schedule.all
+  end
+
+  def show
+  end
+
+  private
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def schedule_params
+    params.require(:schedule).permit( :date,
+                                :uploaded,
+                                :service_id,
+                                :spreadsheet
+                               )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :services
   resources :epgs
+  resources :schedules
+
+  # get 'schedules' ,to: 'schedules#index'
+  # get 'schedules/:id', to: 'schedules#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
