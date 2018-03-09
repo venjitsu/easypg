0d66ed16be8dd6c089fe48a058acc736c4ed8f20
Phil Tek <venjitsu66@gmail.com>
2018-03-09 22:42:06 +0000

    Add first and last names to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  private
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
