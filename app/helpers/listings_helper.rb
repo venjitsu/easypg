b3be60ae7cbe67a1107d815c23ce07de9834dbf5
Phil Tek <venjitsu66@gmail.com>
2018-03-19 20:07:18 +0000

    Start refactoring and expanding ListingsHelper

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,17 +1,31 @@
 module ListingsHelper
-  def hours_in_thirty_mins start_time = nil
+  def grid_increment #In minutes
+    30
+  end
+
+  def listings_times start_time = nil
     range = [] << start_time ||= Time.new(2000,1,1,6,0,0)
     47.times do |i|
-      range << range.last + (60 * 30)
+      range << range.last + (60 * grid_increment)
     end
     range
   end
 
   def number_of_listings_slides
-    hours_in_thirty_mins.count / 4
+    listings_times.count / 4
   end
 
   def clock_time time_object
     time_object.strftime("%H:%M:%S")
+  end
+
+  # TODO Implement everything from here
+  
+  def adjust_duration duration
+    grid_increment / duration
+  end
+
+  def grid_size duration
+    grid_html = adjust_duration duration
   end
 end
